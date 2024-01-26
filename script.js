17bec848668b61bcba4d9e9c0c1db5b03699c1b4
Cassidy <cassidy0kovell@gmail.com>
2024-01-25 18:28:59 -0800

    fixed the assignment of classes and changed the way I was storing data and what was being saved so that I could better assign classes to the time

--- script.js
+++ script.js
@@ -3,25 +3,37 @@ $(function () {
     var dateTimeElement = $('#currentDay');
     setInterval(function() {
       var today = dayjs();
-      var formattedDateTime = today.format('MMMM D, YYYY HH:mm:ss');
+      var formattedDateTime = today.format('MMMM D, YYYY hh:mm:ss A');
       dateTimeElement.text(formattedDateTime);
     }, 1000);
   });
-    var containerEl = $("#time-block-container");
-    var hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-    for (var i = 0; i < hours.length; i++) {
-      var hour = hours[i];
-      var hourId = "hour-" + (i + 9);
-    
-      var timeBlock = $('<div id="' + hourId + '" class="row time-block">');
-      var hourDiv = $('<div class="col-2 col-md-1 hour text-center py-3">').text(hour);
-      var textarea = $('<textarea class="col-8 col-md-10 description" rows="3">');
-      var saveBtn = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">').html('<i class="fas fa-save" aria-hidden="true"></i>');
-    
-      timeBlock.append(hourDiv, textarea, saveBtn);
-      containerEl.append(timeBlock);
+
+  var containerEl = $("#time-block-container");
+  var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17,]
+  function getTimeBlockStatus(hour) {
+    var currentHour = dayjs().hour();
+
+    if (hour < currentHour) {
+      return "past";
+    } else if (hour === currentHour) {
+      return "present";
+    } else {
+      return "future";
     }
-  });
+  }
+
+  for (var i = 0; i < hours.length; i++) {
+    var hour = hours[i];
+    var hourId = "hour-" + i;
+    var status = getTimeBlockStatus(hour);
+    var timeBlock = $('<div id="' + hourId + '" class="row time-block ' + status + '">');
+    var hourDiv = $('<div class="col-2 col-md-1 hour text-center py-3">').text(dayjs('1/1/1 ' + hour + ':00:00').format('hA'));
+    var textarea = $('<textarea class="col-8 col-md-10 description" rows="3">');
+    var saveBtn = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">').html('<i class="fas fa-save" aria-hidden="true"></i>');
+
+    timeBlock.append(hourDiv, textarea, saveBtn);
+    containerEl.append(timeBlock);
+  }
 
   $(document).ready(function() {
     $('.saveBtn').click(function() {
@@ -29,7 +41,7 @@ $(function () {
       var savedText = $(this).siblings('.description').val();
       localStorage.setItem(timeBlockId, savedText);
     });
-  
+
     $('.time-block').each(function() {
       var timeBlockId = $(this).attr('id');
       var savedText = localStorage.getItem(timeBlockId);
@@ -38,6 +50,8 @@ $(function () {
       }
     });
   });
+});
+
   
 
  
